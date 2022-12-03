60a3b7894ba4a6c107bfeb466ffb42a0d84ee1b2
DanielCosta30 <nielcosta24@gmail.com>
2022-12-03 19:13:53 -0300

    feat(script) finalizado codigo javascript

---
+++ js/script.js
@@ -0,0 +1,64 @@
+var controle = document.querySelectorAll("[data-controle]");
+var estatistica = document.querySelectorAll("[data-estatistica]");
+var pecas = {
+
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+
+}
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento)=> {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca)
+        
+    })
+
+})
+
+function manipulaDados (operacao, controle) {
+    var peca = controle.querySelector("[data-contador]")
+
+    if(operacao ==="-"){
+        peca.value = parseInt(peca.value) - 1
+    }else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+
+function atualizaEstatisticas(peca) {
+
+    estatistica.forEach( (elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]       
+    })
+}
